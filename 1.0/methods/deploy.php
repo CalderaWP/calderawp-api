a4c16db591a166416a0e83fc982923d73d99beed
David Cramer <david@digilab.co.za>
2015-02-07 14:27:35 +0200

    allow and auth

--- 1.0/methods/deploy.php
+++ 1.0/methods/deploy.php
@@ -3,10 +3,31 @@
 /// DEPLOY method - ready
 $data = json_decode( file_get_contents( 'php://input' ), true );
 
-ob_start();
-var_dump( $data );
-$out = ob_get_clean();
 
-error_log( $out );
+// authorized senders
+$auths = array(
+	'Desertsnowman',
+);
 
-return array('success' => true );
+
+// allowed deployments
+$deploy = array(
+	'calderawp-api'			=>	'/var/api/calderawp-api',
+	'cwp-theme'				=>	'/var/sites/calderawp.com/wp-content/themes/cwp-theme',
+);
+
+
+if( in_array( $data['sender']['login'], $auths ) && isset( $deploy[ $data['repository']['name'] ] ) ){
+
+	// yup - 
+	//do the git
+
+	exec( "git -C " . $deploy[ $data['repository']['name'] . " pull", $output );
+	
+	error_log( $output );
+	return array('success' => true );
+}
+
+
+
+return array( 'success' => false );
