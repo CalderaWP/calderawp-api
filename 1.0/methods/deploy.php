0da79d7470524c00ab7ce11978c25c3fd4421e22
David Cramer <david@digilab.co.za>
2015-02-09 09:17:06 +0200

    no need to log output

--- 1.0/methods/deploy.php
+++ 1.0/methods/deploy.php
@@ -48,7 +48,7 @@ if( in_array( $data['sender']['login'], $auths ) && isset( $deploy[ $data['repos
 			create_task( 'http_composer_update', array( $path ) );
 		}
 
-		return array('success' => true, 'data' => $data, 'log' => $output );
+		return array('success' => true, 'data' => $data );
 
 	}else{
 		error_log( 'Deploy Not set on ' . $data['repository']['name'] . ' for branch ' . basename( $data['ref'] ) );
