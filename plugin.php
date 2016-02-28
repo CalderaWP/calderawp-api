9b8b0bcaaa3361939098a17a5379a76b55958714
Josh Pollock <jpollock412@gmail.com>
2016-02-28 18:38:58 -0500

    Fix loading check

--- plugin.php
+++ plugin.php
@@ -2,15 +2,16 @@
 /**
 Plugin Name: CalderaWP API
  */
+add_action( 'plugins_loaded', function() {
+	if ( class_exists( 'WP_REST_Posts_Controller' ) ) {
+		if ( file_exists( dirname( __FILE__ ) . '/vendor/autoload.php' ) ) {
+			include_once( dirname( __FILE__ ) . '/vendor/autoload.php' );
+			$api_namespace = 'calderawp_api';
 
-if (  defined( 'REST_API_VERSION' ) &&  version_compare( '2.0-beta2', REST_API_VERSION, '>=' ) )  {
-	if ( file_exists( dirname( __FILE__ ) . '/vendor/autoload.php' ) ) {
-		include_once( dirname( __FILE__ ) . '/vendor/autoload.php' );
-		$api_namespace = 'calderawp_api';
-
-		$version = 'v2';
-		new \calderawp\calderawp_api\boot( $api_namespace, $version );
+			$version = 'v2';
+			new \calderawp\calderawp_api\boot( $api_namespace, $version );
+		}
 	}
-}
+});
 
 
